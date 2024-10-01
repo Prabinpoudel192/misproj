e0bb1f30bc33486b8cd24e78ec30a4eacb2e5ea3
Prabin Poudel <prabinpoudel192@gmail.com>
2024-10-01 14:38:10 +0545

    Upload page correction

--- upload.php
+++ upload.php
@@ -9,7 +9,9 @@
 <body>
     <div id="pra7">
         <form action="" method="post" enctype="multipart/form-data">
-            <textarea placeholder="Your Comment" name="comment" style="height:75px; width:90%; margin:10px"></textarea><br><br>
+        <input type="text" name="name" placeholder="name" style="margin:10px;height:30px;width:90%;border-radius:15px; text-align:center;font-size:20px;"><br>
+        <input type="text" name="price" placeholder="price" style="margin:10px;height:30px;width:90%;border-radius:15px; text-align:center;font-size:20px;"><br>
+        <textarea placeholder="Product Description" name="comment" style="height:75px; width:90%; margin:10px"></textarea><br><br>
             <input type="file" name="file" accept="image/*"><br><br>
             <input type="submit" value="post" name="post" style="height:40px; width:60px; margin:40px 0px 0px 150px;">
         </form>
@@ -19,9 +21,31 @@
 
 <?php
 if(isset($_POST['post'])){
+    $pname=$_POST['name'];
+    $pprice=$_POST['price']; 
+    $pdes=$_POST['comment'];
     $file_name=$_FILES['file']['name'];
     $file_tmp=$_FILES['file']['tmp_name'];
-    move_uploaded_file($file_tmp,"./files/".$file_name);
+    $file_folder="files/".$file_name;
+   if(move_uploaded_file($file_tmp,$file_folder)){
+    $con=new mysqli("localhost","root","","mis");
+    if($con->connect_error){
+        die("No connection");
+    }else{
+        $sql="insert into uploads(pname,pprice,pdes,fname)values('$pname','$pprice','$pdes','$file_folder')";
+        $r=$con->query($sql);
+        if($r){
+            echo "<script>alert('Your product is uploaded for advertisement')</script>";
+        }else{
+            echo "<script>alert('Failed to upload')</script>";
+        }
+        mysqli_close($con);
+    }
+   }else{
+    echo "<script>alert('File upload unsuccessful')</script>";
+    mysqli_close($con);
+   }
+ echo "<script>window.location.href = window.location.href;</script>";
 }
 ?> 
 
