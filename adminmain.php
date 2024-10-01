6727d9fb7d3bc369e17ea9032ccac32ff54391a1
Prabin Poudel <prabinpoudel192@gmail.com>
2024-10-01 06:57:42 +0545

    Admin page setup with the display content

--- adminmain.php
+++ adminmain.php
@@ -8,10 +8,24 @@
 </head>
 <body>
     <div id="pra6">
-        <ul><li><a href="upload.php"><div style="height:35px; width:40px; border:2px solid green; margin:-20px 0px 20px 20px; background-image:url('upload.jpg'); background-size:cover;">
+        <ul>
+    <li><a href="index.php"><div style="height:35px; width:40px; border:2px solid green; margin:-20px 0px 20px 20px; background-image:url('images/logout.png'); background-size:cover;">
         </div></a></li>
-        <li><a href="index.php"><div style="height:35px; width:40px; border:2px solid green; margin:-20px 0px 20px 20px; background-image:url('logout.png'); background-size:cover;">
-        </div></a></li></ul>
+        <li><a href="upload.php"><div style="height:35px; width:40px; border:2px solid green; margin:-20px 0px 20px 20px; background-image:url('images/upload.jpg'); background-size:cover;">
+        </div></a></li>
+    </ul>
+    </div>
+    <div id="pra11">
+        <div id="pra12"></div>
+        <div id="pra13">
+            <h3 style="text-align:center;">Ball Pen</h4>
+            <h5 style="text-style:Bold; margin:10px;">
+                Price:Rs.100<br>
+                Status:Available<br>
+
+            </h6>
+        </div>
+
     </div>
 </body>
 </html>
