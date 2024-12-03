ce7d05a7bfc2ee2a97e6e965c6a1a5d0d30b54f5
Prabin Poudel <prabinpoudel192@gmail.com>
2024-12-03 07:41:25 +0545

    Completed the esewa integration and confirm the payment success test

--- esewa.php
+++ esewa.php
@@ -13,7 +13,7 @@ if (isset($_GET['pid'])) {
             $pprice=$row['pprice'];
             $serchar=$row['serchar'];
             $tax=$row['tax'];
-            $total=$row['total']; 
+            $total=$row['total'];
         }else{
             echo "<script>alert('no data found')</script>";
         }
@@ -36,14 +36,15 @@ if (isset($_GET['pid'])) {
 <body>
 <?php
     
-    $tranuid = "ThankYou" . $pid . time();
+    $tranuid = $pid . time();
     $signed_field_names = "total_amount,transaction_uuid,product_code";
-    $signature_string = $total.",".$tranuid.",EPAYTEST";
-    $secret_key = "8gBm/:&EnhH.1/q("; 
+    $signature_string ="total_amount={$total},transaction_uuid={$tranuid},product_code=EPAYTEST";
+    $secret_key = "8gBm/:&EnhH.1/q"; 
     $s = hash_hmac('sha256', $signature_string, $secret_key, true);
     $signature = base64_encode($s);
     ?>
-    <div id="pra15">
+     
+   <div id="pra15">
     <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST">
  <input type="text" id="amount" name="amount" value="<?php echo $pprice ?>" required>
  <input type="text" id="tax_amount" name="tax_amount" value ="<?php echo $tax ?>" required>
@@ -52,11 +53,11 @@ if (isset($_GET['pid'])) {
  <input type="text" id="product_code" name="product_code" value ="EPAYTEST" required>
  <input type="text" id="product_service_charge" name="product_service_charge" value="<?php echo $serchar ?>" required>
  <input type="text" id="product_delivery_charge" name="product_delivery_charge" value="0" required>
- <input type="text" id="success_url" name="success_url" value="https://brp.com.np/esewa/success.php?q=su" required>
- <input type="text" id="failure_url" name="failure_url" value="https://brp.com.np/esewa/success.php?q=su" required>
+ <input type="text" id="success_url" name="success_url" value="https://localhost/misproj/sucess.php" required>
+ <input type="text" id="failure_url" name="failure_url" value="https://localhost/misproj/supermain.php" required>
  <input type="text" id="signed_field_names" name="signed_field_names" value="<?php echo $signed_field_names ?>" required>
  <input type="text" id="signature" name="signature" value="<?php echo $signature ?>" required>
  <input value="Submit" type="submit">
- </form></div>
+ </form></div> 
 </body>
 </html>
